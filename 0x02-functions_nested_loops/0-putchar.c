3d3ee0a5a20daa5cbd39444351cc318547f519b2
Falana Tolu <falanaft@gmail.com>
2022-03-15 17:13:15 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
@@ -6,13 +5,15 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-
-char *s = "_putchar"                                                         
+{
+char *s = "_putchar";
 
 while(*s != '\0')
 {
 _putchar(*s);
+s++;
 }
 _putchar('\n');
 
 return (0);
+}
