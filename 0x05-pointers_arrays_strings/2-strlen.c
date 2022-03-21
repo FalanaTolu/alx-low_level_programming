81dcd774d1fb73ef66d221e564e85d881f7acee9
Falana Tolu <falanaft@gmail.com>
2022-03-21 12:40:52 -0700

    Add 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string
+* @s: string with unknown length
+* Return: length of string
+*/
+
+int _strlen(char *s)
+{
+
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+j = i + 1;
+}
+return (j);
+}
