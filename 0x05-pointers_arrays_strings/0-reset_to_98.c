d3e7348b9b86c17558d7b749a1ab56a40f551f27
Falana Tolu <falanaft@gmail.com>
2022-03-21 17:38:58 -0700

    Uodate 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,6 @@
 * reset_to_98 - takes a pointer to an int as parameter and updates
 * the value it points to 98
 * @n: integer dereferenced by pointer
-* Return: Always 0
 */
 
 void reset_to_98(int *n)
