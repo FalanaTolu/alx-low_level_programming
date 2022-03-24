9540fcd2c43d49154fe20392952cbbbc9dcf039c
Falana Tolu <falanaft@gmail.com>
2022-03-23 18:49:30 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+int i = 0;
+int j = 0;
 
-i = 0;
-while (*(src + i) != '\0')
+while (*(dest + i) != '\0')
 {
 i++;
 }
 
-j = 0;
 while (*(src + j) != '\0')
 {
 *(dest + i) = *(src + j);
