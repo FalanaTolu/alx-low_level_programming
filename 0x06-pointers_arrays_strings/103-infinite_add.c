a16d93bdcf08b26d2cb06c318eaf4a95f645bf78
Falana Tolu <falanaft@gmail.com>
2022-03-25 17:12:27 -0700

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,7 +11,7 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j, k, l, m;
+int i, j, k, g, l, m1, m2;
 
 i = 0;
 j = 0;
@@ -20,33 +20,44 @@ i++;
 while (*(n2 + j) != '\0')
 j++;
 
-if (i > size_r || j > size_r)
+if (i >= j)
+k = i;
+else
+k = j;
+if (size_r <= k + 1)
 return (0);
+
+*(r + k + 1) = '\0';
 
 i--;
 j--;
 size_r--;
-l = 0;
-for (k = 0; k < size_r; k++, i--, j--)
-{
-if (i >= 0)
-l += *(n1 + i) - '0';
-if (j >= 0)
-l += *(n2 + j) - '0';
-if (i < 0 && j < 0 && l == 0)
-break;
+m1 = *(n1 + i) - 48;
+m2 = *(n2 + j) - 48;
 
-*(r + k) = l % 10 + '0';
-l /= 10;
-}
-*(r + k) = '\0';
-if (i >= 0 || j >= 0 || l)
-return (0);
-for (k--, m = 0; k > m; k--, m++)
+while (k >= 0)
 {
-l = *(r + k);
-*(r + k) = *(r + m);
-*(r + m) = l;
+l = m1 + m2 + g;
+if (l >= 10)
+g = l / 10;
+else
+g = 0;
+if (l > 0)
+*(r + k) = (l % 10) + 48;
+else
+*(r + k) = '0';
+if (i > 0)
+i--, m1 = *(n1 + i) - 48;
+else
+m1 = 0;
+if (j > 0)
+j--, m2 = *(n2 + j) - 48;
+else
+m2 = 0;
+k--, size_r--;
 }
+if (*(r) == '0')
+return (r + 1);
+else
 return (r);
 }
