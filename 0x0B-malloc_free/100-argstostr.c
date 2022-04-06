c9ecdde15264614cb2f35f1e0c8ac8be55f93b68
Falana Tolu <falanaft@gmail.com>
2022-04-06 12:02:08 -0700

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,13 +13,14 @@ char *argstostr(int ac, char **av)
 char *s;
 int i, j, k, l;
 k = 0;
-l = 0;
+
 if (ac == 0 || av == NULL)
 return (NULL);
 
 for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
+l++;
 l++;
 }
 
