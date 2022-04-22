770e50f0ba18086e9c8c316a9f6e28d65fe1983f
Falana Tolu <falanaft@gmail.com>
2022-04-22 15:35:10 -0700

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 
 /**
 * *add_node - adds a new node at the beginning of a list_t list
-* @head: linked list 
+* @head: linked list
 * @str: string to be added to the list
 * Return: address of the new element, or NULL if it failed
 */
