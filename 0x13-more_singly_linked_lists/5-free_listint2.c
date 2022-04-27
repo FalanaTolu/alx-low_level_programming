315816889084e9b06272fd4eff6961829ec53f7e
Falana Tolu <falanaft@gmail.com>
2022-04-26 17:43:37 -0700

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,9 +7,15 @@
 
 void free_listint2(listint_t **head)
 {
+listint_t *tmp;
+
 if (head == NULL)
 return;
-free_listint((*head)->next);
+while (*head != NULL)
+{
+tmp = (*head)->next;
 free(*head);
+*head = tmp;
+}
 *head = NULL;
 }
